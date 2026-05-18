3c7d20e362c61d4e0711a2aa4ce8d0e0e37b3337
madhushalini23 <gundlurumadhushalini23@gmail.com>
2026-05-18 15:42:44 +0530

    expense shell

--- mysql.sh
+++ mysql.sh
@@ -42,7 +42,7 @@ VALIDATE $? "enabling mysql server"
 systemctl start mysqld &>>$LOG_FILE_NAME
 VALIDATE $? "starting mysql server"
 
-mysql_secure_installation --set-root-pass ExpennseApp@1
+mysql_secure_installation --set-root-pass ExpenseApp@1
 VALIDATE $? "setting mysql root password"
 
 
