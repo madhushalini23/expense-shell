00b84a0d66baf181f1ffc36c20889209a61f468b
madhushalini23 <gundlurumadhushalini23@gmail.com>
2026-05-20 11:03:33 +0530

    expense shell

---
+++ frontend.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"]
+G="\e[32m"]
+Y="\e[33m"]
+N="\e[0m"]
+
+LOGS_FOLDER="/var/log/expense-logs"
+LOG_FILE=$(echo $0| cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2... $R failure $N"
+        exit 1
+    else
+        echo -e "$2... $G success $N"
+    fi
+}
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "ERROR:: you must have sudo access to execute this script"
+        exit 1
+    fi
+}
+
+echo "script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+CHECK_ROOT
+
+dnf install nginx -y &>>$LOG_FILE_NAME
+VALIDATE $? "installing nginx"
+
+systemctl enable nginx &>>$LOG_FILE_NAME
+VALIDATE $? "enabling nginx"
+
+systemctl start nginx &>>$LOG_FILE_NAME
+VALIDATE $? "starting nginx"
+
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE_NAME
+VALIDATE $? "removing existing version of code"
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
+VALIDATE $? "downloading frontend code"
+
+cd /usr/share/nginx/html &>>$LOG_FILE_NAME
+VALIDATE $? "Moving to html directory"
+
+unzip /tmp/frontend.zip &>>$LOG_FILE_NAME
+VALIDATE $? "unzipping frontend code"
+
+systemctl restart nginx &>>$LOG_FILE_NAME
+VALIDATE $? "restarting nginx"
+
